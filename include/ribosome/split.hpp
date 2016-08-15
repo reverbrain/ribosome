7cd64f96d64458c9f502e15d85ccbe31aad2b065
Evgeniy Polyakov <zbr@ioremap.net>
2016-08-15 17:41:38 +0300

    split: the last argument to convert_split_words() is an alphabet to split text on. This is additional to unicode word delimiters set.

--- include/ribosome/split.hpp
+++ include/ribosome/split.hpp
@@ -4,7 +4,9 @@
 #include "ribosome/charset.hpp"
 #include "ribosome/lstring.hpp"
 
+#include <string>
 #include <vector>
+#include <unordered_map>
 
 #include <unicode/ubrk.h>
 
@@ -12,13 +14,34 @@ namespace ioremap { namespace ribosome {
 
 class split {
 public:
-	std::vector<lstring> convert_split_words(const char *text, size_t size, const std::string &enc_hint) {
+	std::vector<lstring> convert_split_words(const char *text, size_t size, const std::string &drop) {
 		std::vector<UChar> uc(size+1);
 
 		size_t usize = uc.size();
 
-		m_ch.convert((UChar *)uc.data(), &usize, text, size, enc_hint);
+		m_ch.convert((UChar *)uc.data(), &usize, text, size);
 		uc.resize(usize);
+
+		if (drop.size()) {
+			std::vector<UChar> drop_chars(drop.size() + 1);
+			size_t dsize = drop_chars.size();
+
+			m_ch.convert((UChar *)drop_chars.data(), &dsize, drop.data(), drop.size());
+			drop_chars.resize(dsize);
+
+			std::unordered_map<UChar, int> m;
+			for (size_t i = 0; i < dsize; ++i) {
+				m[drop_chars[i]] = 1;
+			}
+
+			static UChar space = ' ';
+			for (size_t i = 0; i < uc.size(); ++i) {
+				auto it = m.find(uc[i]);
+				if (it != m.end()) {
+					uc[i] = space;
+				}
+			}
+		}
 
 		std::vector<lstring> ret;
 
@@ -37,6 +60,9 @@ public:
 				prev = pos;
 			} else {
 				lstring tmp = lconvert::from_unicode(uc.data() + prev, pos - prev);
+				for (auto it = tmp.begin(); it != tmp.end(); ++it) {
+				}
+
 				ret.emplace_back(tmp);
 
 				prev = -1;
