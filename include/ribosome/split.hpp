228ec57c96e6d1d9755da0f5e8631a66812d1f32
Evgeniy Polyakov <zbr@ioremap.net>
2016-08-19 18:31:50 +0300

    split: use lstring when doing split

--- include/ribosome/split.hpp
+++ include/ribosome/split.hpp
@@ -1,7 +1,6 @@
 #ifndef __RIBOSOME_SPLIT_HPP
 #define __RIBOSOME_SPLIT_HPP
 
-#include "ribosome/charset.hpp"
 #include "ribosome/lstring.hpp"
 
 #include <string>
@@ -14,31 +13,31 @@ namespace ioremap { namespace ribosome {
 
 class split {
 public:
-	std::vector<lstring> convert_split_words(const char *text, size_t size, const std::string &drop) {
-		std::vector<UChar> uc(size+1);
-
-		size_t usize = uc.size();
-
-		m_ch.convert((UChar *)uc.data(), &usize, text, size);
-		uc.resize(usize);
+	std::vector<lstring> convert_split_words(const lstring &lt, const std::string &drop) {
+		lstring copy;
+		lstring *ptr = (lstring *)&lt;
 
 		if (drop.size()) {
-			std::vector<UChar> drop_chars(drop.size() + 1);
-			size_t dsize = drop_chars.size();
+			lstring ld = lconvert::from_utf8(drop);
 
-			m_ch.convert((UChar *)drop_chars.data(), &dsize, drop.data(), drop.size());
-			drop_chars.resize(dsize);
-
-			std::unordered_map<UChar, int> m;
-			for (size_t i = 0; i < dsize; ++i) {
-				m[drop_chars[i]] = 1;
+			std::unordered_map<letter, int, letter_hash> m;
+			for (auto &ch: ld) {
+				m[ch] = 1;
 			}
 
-			static UChar space = ' ';
-			for (size_t i = 0; i < uc.size(); ++i) {
-				auto it = m.find(uc[i]);
+			bool changed = false;
+			static letter space(' ');
+			for (size_t i = 0; i < copy.size(); ++i) {
+				auto it = m.find(lt[i]);
 				if (it != m.end()) {
-					uc[i] = space;
+					if (!changed) {
+						copy = lt;
+						ptr = &copy;
+
+						changed = true;
+					}
+
+					copy[i] = space;
 				}
 			}
 		}
@@ -49,7 +48,7 @@ public:
 		int prev = -1, pos;
 
 		UErrorCode err = U_ZERO_ERROR;
-		bi = ubrk_open(UBRK_WORD, get_locale(), uc.data(), uc.size(), &err);
+		bi = ubrk_open(UBRK_WORD, get_locale(), (UChar *)ptr->data(), ptr->size(), &err);
 		if (U_FAILURE(err))
 			return ret;
 
@@ -59,11 +58,7 @@ public:
 			if ((rules == UBRK_WORD_NONE) || (prev == -1)) {
 				prev = pos;
 			} else {
-				lstring tmp = lconvert::from_unicode(uc.data() + prev, pos - prev);
-				for (auto it = tmp.begin(); it != tmp.end(); ++it) {
-				}
-
-				ret.emplace_back(tmp);
+				ret.emplace_back(ptr->substr(prev, pos - prev));
 
 				prev = -1;
 			}
@@ -76,12 +71,16 @@ public:
 		return ret;
 	}
 
+	std::vector<lstring> convert_split_words(const char *text, size_t size, const std::string &drop) {
+		lstring lt = ribosome::lconvert::from_utf8(text, size);
+		return convert_split_words(lt, drop);
+	}
+
 	std::vector<lstring> convert_split_words(const char *text, size_t size) {
 		return convert_split_words(text, size, "");
 	}
 
 private:
-	charset m_ch;
 };
 
 }} // namespace ioremap::ribosome
